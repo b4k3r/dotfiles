f6369447a6e50dd34a06968302f64bd3e2e922fe
Marcin Prokop <marcin@prokop.co>
2017-02-19 11:23:57 +0100

    Add vim to install script

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ echo "Installing Homebrew ..."
 
 echo "Installing dependencies ..."
 brew install zsh git cmake ack ctags tmux gpg
+brew install macvim --with-override-system-vim
 
 echo "Installing RVM .."
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
