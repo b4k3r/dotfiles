1a8f373e4daf03f8febb79b1c2c8db630a672855
Marcin Prokop <marcin@prokop.co>
2020-04-19 10:49:32 +0200

    Make compatible with Ubuntu Focal

--- install_linux.sh
+++ install_linux.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Installing dependencies ..."
-sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get -qq update
 sudo apt-get install -yq git \
                          silversearcher-ag \
@@ -13,13 +12,29 @@ sudo apt-get install -yq git \
                          ack-grep \
                          ctags \
                          tmux \
+                         docker.io \
+                         docker-compose \
+                         fd-finx \
+                         fzf \
                          gnupg2 \
+                         neovim \
+                         scdaemon \
                          tig \
                          python-dev \
                          python-pip \
                          python3-dev \
                          python3-pip >/dev/null 2>&1
-curl -sL install-node.now.sh/lts | sudo bash
+
+sudo usermod -aG docker $USER
+
+echo "Installing Node ..."
+sudo snap install node --channel=12/stable --classic
+
+echo "Installing Golang ..."
+sudo snap install go --channel=1.13/stable --classic
+
+echo "Installing Heroku CLI ..."
+sudo snap install --classic heroku
 
 echo "Coping files ..."
 mkdir ~/.fonts
@@ -31,14 +46,6 @@ for item in `git ls-files` ; do
 done
 rm ~/.install_*.sh ~/.update.sh
 
-echo "Installin FZF ..."
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-echo "Installin fd finder ..."
-wget https://github.com/sharkdp/fd/releases/download/v7.5.0/fd_7.5.0_amd64.deb -P /tmp
-sudo dpkg -i /tmp/fd_7.5.0_amd64.deb
-
 echo "Installing vim-plug ..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -46,28 +53,13 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 echo "Installing plugins ...."
 vim +PlugInstall
 
-echo "Installing Docker ..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository -u \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get install -yq docker-ce docker-ce-cli containerd.io >/dev/null 2>&1
-sudo usermod -aG docker $USER
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo "Installing Docker Compose ..."
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Installing RVM ..."
-gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-echo "Installing Heroku CLI ..."
-sudo snap install --classic heroku
-
-echo "Installing Golang ..."
-wget https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz -P /tmp
-sudo tar -C /usr/local -xzf /tmp/go1.14.1.linux-amd64.tar.gz
-mkdir -p ~/go
 
 echo "Installing zsh ..."
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
