d7d6ec20b2a9ac7a61cdf1ced80e78fc56c2a33b
Marcin Prokop <marcin@prokop.co>
2020-04-30 21:15:27 +0200

    Bye bye Ubuntu, hello Fedora

--- install_linux.sh
+++ install_linux.sh
@@ -1,50 +1,47 @@
 #!/bin/bash
 
 echo "Installing dependencies ..."
-# Youbikey
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 32CBA1A9
-sudo add-apt-repository ppa:yubico/stable
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install nodejs \
+                 zsh \
+                 neovim \
+                 python3-neovim \
+                 the_silver_searcher \
+                 fd-find \
+                 ack \
+                 nodejs \
+                 ctags \
+                 tig \
+                 tmux \
+                 yubioath-desktop \
+                 java-11-openjdk \
+                 util-linux-user \
+                 grubby \
+                 wireshark \
+                 docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker,wireshark $USER
+sudo alternatives --config java
 
-# Insync
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
-sudo add-apt-repository "deb http://apt.insync.io/ubuntu focal non-free contrib"
+# https://fedoraproject.org/wiki/Common_F31_bugs#Other_software_issues
+sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
 
-# Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu eoan stable"
+sudo systemctl start docker
+sudo systemctl enable docker
 
-sudo apt-get -qq update
-sudo apt-get install -yq git \
-                         silversearcher-ag \
-                         neovim \
-                         gpg-agent \
-                         curl \
-                         zsh \
-                         build-essential \
-                         cmake \
-                         ack-grep \
-                         ctags \
-                         tmux \
-                         openjdk-11-jdk \
-                         maven \
-                         gnupg2 \
-                         docker-ce docker-ce-cli containerd.io \
-                         neovim \
-                         insync \
-                         tig \
-                         yubioath-desktop \
-                         python3-dev \
-                         python3-pip
-sudo usermod -aG docker $USER
-
-echo "Installing Node ..."
-sudo snap install node --channel=12/stable --classic
+echo "Installing Maven ..."
+wget https://www-us.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -P /tmp
+sudo tar -C /opt -xzf apache-maven-3.6.3-bin.tar.gz
 
 echo "Installing Golang ..."
-sudo snap install go --channel=1.13/stable --classic
+wget https://dl.google.com/go/go1.13.10.linux-amd64.tar.gz -P /tmp
+sudo tar -C /usr/local -xzf /tmp/go1.13.10.linux-amd64.tar.gz
+mkdir -p ~/go
 
 echo "Installing Heroku CLI ..."
-sudo snap install --classic heroku
+curl https://cli-assets.heroku.com/install.sh | sh
 
 echo "Coping files ..."
 mkdir ~/.fonts
@@ -60,16 +57,12 @@ echo "Installin FZF ..."
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-echo "Installin fd finder ..."
-wget https://github.com/sharkdp/fd/releases/download/v8.0.0/fd_8.0.0_amd64.deb -P /tmp
-sudo dpkg -i /tmp/fd_8.0.0_amd64.deb
-
 echo "Installing vim-plug ..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Installing plugins ...."
-vim +PlugInstall
+nvim +PlugInstall
 
 echo "Installing Docker Compose ..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -81,8 +74,8 @@ curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 echo "Installing zsh ..."
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-rm ~/.zshrc
-ln -s ~/dotfiles/zshrc ~/.zshrc
+git checkout zshrc
+ln -sf ~/dotfiles/zshrc ~/.zshrc
 chsh -s /bin/zsh
 
 echo "Done!"
--- linux.zsh
+++ linux.zsh
@@ -1,14 +1,7 @@
-# Add RVM and Golang
-
-export PATH="$PATH:$HOME/.rvm/bin:/usr/local/go/bin"
+# Add RVM, Golang and Maven
+export PATH="$PATH:$HOME/.rvm/bin:/usr/local/go/bin:/opt/apache-maven-3.6.3/bin"
 
 # Aliases
-
-alias agi='sudo apt-get install'
-alias agr='sudo apt-get --purge autoremove'
-alias agu='sudo apt-get update'
-alias agug='agu && sudo apt-get upgrade'
-alias acs='apt-cache search'
-
+alias vim='nvim'
 alias rm='rm -rf'
 alias s='sudo'
