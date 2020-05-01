331a390020c57b616207d11a7d503911350997bf
Marcin Prokop <marcin@prokop.co>
2020-05-01 15:52:48 +0200

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -5,7 +5,7 @@ sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install nodejs \
+sudo dnf -y install nodejs \
                  zsh \
                  neovim \
                  python3-neovim \
