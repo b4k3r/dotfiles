406c38f0fd2baf4ef824cd3e1b9426ca31f5a85e
Marcin Prokop <marcin@prokop.co>
2021-08-13 23:28:39 +0200

    Bump Go version

--- update.sh
+++ update.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-function latest_tag () {
-  curl -s https://api.github.com/repos/$1/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'
-}
-
-COMPOSE_VER=$(latest_tag 'docker/compose')
-GOLANG_VER="1.16.5"
-SIMPLENOTE_VER=$(latest_tag 'Automattic/simplenote-electron')
-TER_VER=$(latest_tag 'hashicorp/terraform')
+source ./common.sh
 
 echo "Updating files ..."
 git pull
